18cf3a3b84674e332bf189d0d0ce59122961fa47
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-04-18 12:37:44 +0700

    feat: implementing fetchGoogleApi to get book data
    implmenting fetchGoogleAPI function to get book data from ISBN

---
+++ app/controllers/books_controller.ts
@@ -0,0 +1,44 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import axios from 'axios';
+
+export default class BooksController {
+
+  public async fetchGoogleAPI({ request, response }: HttpContext){
+    const {ISBN} = request.body()
+    const url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + ISBN
+    try {
+      const res = await axios.get(url);
+
+      if(res.data.totalItems > 0) {
+        const title: string = res.data.items[0].volumeInfo.title
+        const authors: Array<string> = res.data.items[0].volumeInfo.authors
+        const publisher: string = res.data.items[0].volumeInfo.publisher
+        const publishedDate: string = res.data.items[0].volumeInfo.publishedDate
+
+        const data = {
+          title: title,
+          authors: authors,
+          publisher: publisher,
+          date: publishedDate
+        }
+
+        return response.status(200).json({
+          code: 200,
+          message: "success",
+          data: data
+        })
+      } else {
+        return response.status(200).json({
+          code: 200,
+          message: "not found"
+        })
+      }
+    } catch (error) {
+      return response.status(500).json({
+        code: 500,
+        error: error.message
+      })
+    }
+  }
+
+}
