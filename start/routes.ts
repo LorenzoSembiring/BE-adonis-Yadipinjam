c90d05eb29fc76c8ae3f383a5b8c19b916573438
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-04-19 05:31:36 +0700

    feat book and publisher route

--- start/routes.ts
+++ start/routes.ts
@@ -9,7 +9,9 @@
 
 import router from '@adonisjs/core/services/router'
 import UsersController from '#controllers/users_controller'
+import BooksController from '#controllers/books_controller'
 import AuthorsController from '#controllers/authors_controller'
+import PublishersController from '#controllers/publishers_controller'
 
 router.post('/register', [UsersController, 'register'])
 router.post('/login', [UsersController, 'login'])
@@ -18,3 +20,6 @@ router.post('/fetch-book',[BooksController, 'fetchGoogleAPI'])
 
 router.post('author/store',[AuthorsController, 'store'])
 router.get('author/index',[AuthorsController, 'index'])
+
+router.post('publisher/store',[PublishersController, 'store'])
+router.get('publisher/index',[PublishersController, 'index'])
