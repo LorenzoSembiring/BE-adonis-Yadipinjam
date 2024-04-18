7bf53dba424a61442647327b79d56930b3f7c8b7
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-04-18 16:12:14 +0700

    feat: publisher store and index

---
+++ app/controllers/publishers_controller.ts
@@ -0,0 +1,48 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import db from '@adonisjs/lucid/services/db'
+import Publisher from '#models/publisher'
+
+export default class PublishersController {
+  public async store({ request, response }: HttpContext) {
+    const { name } = request.body()
+    const trx = await db.transaction()
+
+    try {
+      const author = await Publisher.create({
+        name: name
+      })
+
+      await trx.commit()
+      return response.status(200).json({
+        code: 200,
+        message: "success",
+        data: author
+      })
+    } catch (error) {
+      await trx.rollback()
+
+      return response.status(500).json({
+        code: 500,
+        message: "fail",
+        error: error
+      })
+    }
+  }
+  public async index({ response }: HttpContext) {
+    try {
+      const data = await Publisher.all()
+
+      return response.status(200).json({
+        code: 200,
+        message: "success",
+        data: data
+      })
+    } catch (error) {
+      return response.status(500).json({
+        code: 500,
+        message: "fail",
+        error: error
+      })
+    }
+  }
+}
