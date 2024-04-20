2330ab16a696bc3e4161ad133ee8e65325132284
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-04-20 21:50:59 +0700

    feat create book
    implementing store book logic

--- app/controllers/books_controller.ts
+++ app/controllers/books_controller.ts
@@ -1,6 +1,11 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import axios from 'axios';
+import Book from '#models/book'
 import BookAuthor from '#models/book_author'
+import Publisher from '#models/publisher';
+import Author from '#models/author'
+import PublishersController from '#controllers/publishers_controller';
+import AuthorsController from './authors_controller.js';
 
 export default class BooksController {
 
@@ -54,4 +59,56 @@ export default class BooksController {
     }
   }
 
+  //this code bellow will cretae book entity that acted as master data, it will called when someone uploading their book
+  public async createBook(ISBN: string, authors: string[], publisher: string, year: number, title: string) {
+    const publishersController = new PublishersController()
+    const authorsController = new AuthorsController()
+    // const { ISBN, authors, publisher, year, title} = request.body()
+    const trx = await db.transaction()
+
+    const bookData = await Book.findBy("ISBN", ISBN)
+    try{
+      // retrieve publisher data based on name, create if not exist
+      const publisherData = await Publisher.findBy('name', publisher);
+
+      if (publisherData) {
+        var publisher_ID: number | null = publisherData.id
+      } else {
+        var publisher_ID: number | null = await publishersController.create(publisher)
+      }
+
+      //check the publisher not null
+      if (publisher_ID && !bookData) {
+        var book = await Book.create({
+          ISBN: ISBN,
+          title: title,
+          publisher_ID: publisher_ID,
+          year: year
+        })
+      } else {
+        return "Book existed"
+      }
+
+      // retrieve author data based on name, create if not exist
+      // create bookAuthor after author exist
+      for (let index = 0; index < authors.length; index++) {
+        const authorData = await Author.findBy('name', authors[index]);
+        if (authorData) {
+          var author_ID: number | null = authorData.id
+        } else {
+          var author_ID: number | null = await authorsController.create(authors[index])
+        }
+        if (author_ID) {
+          await this.createBookAuthor(ISBN, author_ID)
+        }
+      }
+
+      await trx.commit()
+
+      return book!
+    } catch (error) {
+      await trx.rollback()
+      return null
+    }
+  }
 }
