17938a5438fdd68699f4e5e355ee5abadd9bca3d
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-04-20 23:49:43 +0700

    feat book upload route

--- start/routes/book.ts
+++ start/routes/book.ts
@@ -3,5 +3,6 @@ import BooksController from '#controllers/books_controller'
 
 router.group(() => {
   router.post('/fetch-book',[BooksController, 'fetchGoogleAPI'])
+  router.post('/book/upload',[BooksController, 'uploadBook'])
 
 }).prefix('/api')
