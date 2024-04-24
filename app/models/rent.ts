6d7a11570282dd1cc193b169760f6441011e368c
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-04-24 23:02:18 +0700

    feat: rent model

--- app/models/rent.ts
+++ app/models/rent.ts
@@ -5,6 +5,21 @@ export default class Rent extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
+  @column()
+  declare userID: number
+
+  @column()
+  declare Circulated_BookID: number
+
+  @column()
+  declare status: string
+
+  @column()
+  declare start_date: Date
+
+  @column()
+  declare end_date: Date
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
