e13b961dc7cae6a1f1b6c7b009e2755ecda7d4cc
yasfaa <yasfanaruto@gmail.com>
2024-04-25 13:33:16 +0700

    fix author list user circulated book

--- app/controllers/books_controller.ts
+++ app/controllers/books_controller.ts
@@ -316,7 +316,7 @@ export default class BooksController {
 
     try {
       const query = await db.rawQuery(
-        "SELECT cb.id AS circulated_book_id, b.ISBN, b.title AS book_title, GROUP_CONCAT(a.name SEPARATOR ', ') AS authors, p.name AS publisher, cb.description, cp.path AS image_link, cb.status as status FROM circulated_books cb JOIN books b ON cb.books_ISBN = b.ISBN JOIN book_authors ba ON b.ISBN = ba.books_ISBN JOIN authors a ON ba.author_ID = a.id JOIN publishers p ON b.publisher_ID = p.id LEFT JOIN circulated_pictures cp ON cb.id = cp.circulated_book_ID JOIN users u ON cb.user_ID = u.id WHERE u.id = :id GROUP BY cb.id, b.ISBN;",
+        "SELECT cb.id AS circulated_book_id, b.ISBN, b.title AS book_title, GROUP_CONCAT(DISTINCT a.name SEPARATOR ', ') AS authors, p.name AS publisher, cb.description, cp.path AS image_link, cb.status AS status FROM circulated_books cb JOIN books b ON cb.books_ISBN = b.ISBN JOIN book_authors ba ON b.ISBN = ba.books_ISBN JOIN authors a ON ba.author_ID = a.id JOIN publishers p ON b.publisher_ID = p.id LEFT JOIN circulated_pictures cp ON cb.id = cp.circulated_book_ID JOIN users u ON cb.user_ID = u.id WHERE u.id = :id GROUP BY cb.id, b.ISBN;",
         {
           id: user.id
         }
