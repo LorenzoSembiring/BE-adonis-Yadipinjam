745aa15e1239ca5f8ea8c3501bd85fc402fbb105
yasfaa <yasfanaruto@gmail.com>
2024-05-16 12:08:54 +0700

    fix confirm rent

--- database/migrations/1713347822124_create_rents_table.ts
+++ database/migrations/1713347822124_create_rents_table.ts
@@ -8,7 +8,7 @@ export default class extends BaseSchema {
       table.increments('id')
       table.integer('userID').references('users.id').notNullable().unsigned()
       table.integer('Circulated_BookID').references('circulated_books.id').notNullable().unsigned()
-      table.enum('status', ['pending', 'confirmed', 'overdue', 'returned', 'checking', 'complete'])
+      table.enum('status', ['pending', 'ongoing', 'overdue', 'returned', 'checking', 'complete'])
       table.date('start_date')
       table.date('end_date')
       table.dateTime('created_at')
