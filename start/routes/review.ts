d6bebf4be75ba14855d20ce1188865924ded3824
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-05-29 21:16:11 +0700

    fix: change post to get in route

--- start/routes/review.ts
+++ start/routes/review.ts
@@ -4,7 +4,7 @@ import ReviewsController from '#controllers/reviews_controller'
 router.group(() => {
   router.post('owner',[ReviewsController, 'reviewOwner'])
   router.post('renter',[ReviewsController, 'reviewRenter'])
-  router.post('get/:id',[ReviewsController, 'getReview'])
-  router.post('get/owner/:id',[ReviewsController, 'getOwnerReview'])
-  router.post('get/renter/:id',[ReviewsController, 'getRenterReview'])
+  router.get('get/:id',[ReviewsController, 'getReview'])
+  router.get('get/owner/:id',[ReviewsController, 'getOwnerReview'])
+  router.get('get/renter/:id',[ReviewsController, 'getRenterReview'])
 }).prefix('/api/review')
