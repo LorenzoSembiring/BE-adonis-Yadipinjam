978541e7f07fa4b2eceaba951ec5f2dc53707f13
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-05-31 19:30:09 +0700

    feat: create book review

--- app/controllers/reviews_controller.ts
+++ app/controllers/reviews_controller.ts
@@ -1,6 +1,7 @@
 import UserReview from '#models/user_review'
 import { HttpContext } from '@adonisjs/core/http'
 import db from '@adonisjs/lucid/services/db'
+import BookReview from '#models/book_review'
 
 export default class ReviewsController {
   public async reviewOwner({ auth, request, response }: HttpContext) {
@@ -219,6 +220,48 @@ export default class ReviewsController {
         status: 'success',
         data: review[0][0],
       })
+    } catch (error) {
+      return response.status(500).json({
+        code: 500,
+        message: 'Failed',
+        error: error.message,
+      })
+    }
+  }
+
+  public async createBookReview({ auth, request, response }: HttpContext) {
+    const user = await auth.authenticate()
+    const { circulated_ID, star, review } = request.body()
+    try {
+      const hasBorrowed = await db.rawQuery(
+        'SELECT r.id FROM `rents` r JOIN `circulated_books` cb ON r.Circulated_BookID = cb.id WHERE cb.id = :book AND r.userID = :renter;',
+        {
+          book: circulated_ID,
+          renter: user.id
+        }
+      )
+
+      // if condition untuk pengecekan bahwa pengisi review adalah benar peminjam buku
+      if (hasBorrowed[0][0]) {
+        const data = await BookReview.create({
+          circulated_id: circulated_ID,
+          feedback: review,
+          star: star,
+          from: user.id
+        })
+
+        return response.status(201).json({
+          code: 201,
+          status: "created",
+          data: data
+        })
+      } else {
+        return response.status(403).json({
+          code: 403,
+          status: "forbidden",
+          message: "You did not borrow this book!"
+        })
+      }
     } catch (error) {
       return response.status(500).json({
         code: 500,
--- start/routes/review.ts
+++ start/routes/review.ts
@@ -4,6 +4,7 @@ import ReviewsController from '#controllers/reviews_controller'
 router.group(() => {
   router.post('owner',[ReviewsController, 'reviewOwner'])
   router.post('renter',[ReviewsController, 'reviewRenter'])
+  router.post('book',[ReviewsController, 'createBookReview'])
   router.get('get/:id',[ReviewsController, 'getReview'])
   router.get('get/owner/:id',[ReviewsController, 'getOwnerReview'])
   router.get('get/renter/:id',[ReviewsController, 'getRenterReview'])
